d8baeeac3a2909db0624eda6cdd2174a0ec2ed54
alex stovall <108300575+Astovall9900@users.noreply.github.com>
2023-06-29 13:53:25 -0500

    template cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
 
-  return (
-    <>
-     Welcome to PieUI
-    </>
-  )
+  return (<>
+    Welcome to Pie UI
+  </>)
 }
-
 export default App
